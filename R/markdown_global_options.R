b111850a4ed65b23ba29507973a87e1214ad0b4a
JP <30301499+jpospina88@users.noreply.github.com>
2019-03-05 18:04:12 -0800

    adds global options

---
+++ R/markdown_global_options.R
@@ -0,0 +1,6 @@
+knitr::opts_chunk$set(
+  # fig.path = 'figs/', fig.show = 'asis', 
+  dpi = 75, 
+  # include = FALSE, 
+  echo = FALSE, # run the code, show me only the graphs
+  warning = FALSE, message = FALSE, cache = FALSE)
